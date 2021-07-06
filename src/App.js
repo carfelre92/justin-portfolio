348e51b37310163a18bec26cacece59b423d0df8
Jin Kim <wlsgns92@hotmail.com>
2021-07-07 04:03:52 +1200

    route and switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import Popper from 'popper.js';
+import { React, Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/App.css';
-
+import About from './component/About';
+import Main from './component/Main';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Popper from 'popper.js';
 import MetaTags from 'react-meta-tags';
-import { Container, Row, Col } from 'react-bootstrap'
 
 
 class App extends Component {
@@ -17,36 +18,13 @@ class App extends Component {
 
   render() {
     return (
-
       <div className="wrapper">
-        <header className="head">
-        </header>
-
-        <div className="section1">
-          <Container className="main-page">
-            <Row>
-              <Col className="">
-                <div className="main-page-name">
-                  <h1>Justin Lee</h1>
-                </div>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={7} className="">
-                <div className="main-page-paragraph">
-                  <h3>Lorem ipsum dolor sit amet, consectetur</h3>
-                  <h3>consectetur adipiscing elit</h3>
-                  <h3>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h3>
-                </div>
-              </Col>
-            </Row>
-            <div className="btn-group">
-              <button className="main-page-button">PROJECTS</button>
-              <button className="main-page-button">ABOUT ME</button>
-            </div>
-          </Container>
-        </div>{/* section1 */}
-
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Main} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/component/About.js
@@ -0,0 +1,43 @@
+import { React, Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+class About extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+                <div className="section1">
+                    <Container className="main-page">
+                        <Row>
+                            <Col className="">
+                                <div className="main-page-name">
+                                    <h1>Its about page tbh</h1>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={7} className="">
+                                <div className="main-page-paragraph">
+                                    <h3>Lorem ipsum dolor sit amet, consectetur</h3>
+                                    <h3>consectetur adipiscing elit</h3>
+                                    <h3>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h3>
+                                </div>
+                            </Col>
+                        </Row>
+                        <div className="btn-group">
+                            <button className="main-page-button">PROJECTS</button>
+                            <button className="main-page-button">ABOUT ME</button>
+                        </div>
+                    </Container>
+                </div>
+        )
+    }
+}
+
+export default About;
